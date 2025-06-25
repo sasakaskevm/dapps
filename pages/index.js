d9f83e294d914a22ec7945ada04b8a78228ba137
sasakaskevm <indraghaul452@gmail.com>
2025-06-25 05:40:40 +0000

    add homepage index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 Welcome to Helios Staking DApp</h1>
+      <p>Frontend connected & deployed successfully to Vercel</p>
+    </div>
+  );
+}
+
