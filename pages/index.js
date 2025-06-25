1ec9987631ba2615abba47c85fbd51d9e9b05133
sasakaskevm <indraghaul452@gmail.com>
2025-06-25 05:50:14 +0000

    fix: remove pages/ folder to resolve conflict with app/ routing

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>🚀 Welcome to Helios Staking DApp</h1>
-      <p>Frontend connected & deployed successfully to Vercel</p>
-    </div>
-  );
-}
-
